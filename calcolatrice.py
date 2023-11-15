2cf33b252b37172a09dc0d2570ec6aa673523234
IlFraHub <francescoumbaca9@gmail.com>
2023-11-15 10:56:09 +0100

    changes

---
+++ calcolatrice.py
@@ -0,0 +1,38 @@
+def calcolatrice():
+    domanda = input("che operazione desideri effettuare? rispondi utilizzando i seguenti operatori (+  -  /  * )\n" )
+    a = int(input("inserici il primo valore \n"))
+    b = int(input("inserici il secondo valore \n"))
+    risultato = 0
+
+    def somma (a,b):
+        return a+b
+
+    def sottrazione(a,b):
+        return a-b
+
+    def moltiplicazione (a,b):
+        return a*b
+
+    def divisione (a,b):
+        return a/b
+
+    # stampa risultati
+    if domanda == "+":
+        risultato = somma(a,b)
+
+    elif domanda == "-":
+        risultato= sottrazione(a,b)
+
+    elif domanda == "*":
+        risultato = moltiplicazione(a, b)
+
+    elif domanda == "/":
+        risultato = divisione(a, b)
+
+    print(risultato)
+
+# execute
+if __name__ == "__main__":
+    calcolatrice()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
